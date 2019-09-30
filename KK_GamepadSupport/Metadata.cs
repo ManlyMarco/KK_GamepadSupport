4bf575b96ea3c2ffef98bee91fa6b38aa6fd1796
ManlyMarco <dummyaccx@gmail.com>
2019-10-01 01:02:37 +0200

    Version up to v1.0

--- KK_GamepadSupport/Metadata.cs
+++ KK_GamepadSupport/Metadata.cs
@@ -2,7 +2,7 @@ namespace KK_GamepadSupport
 {
     internal static class Metadata
     {
-        public const string Version = "0.6";
+        public const string Version = "1.0";
         public const string BaseGuid = "GamepadSupport";
     }
 }
