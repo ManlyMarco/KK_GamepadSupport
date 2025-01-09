ac6938c0c8488d30c76629f8ace4947eb65a155a
superboo07 <39241538+superboo07@users.noreply.github.com>
2025-01-08 20:53:19 -0500

    Make cursor rendering better

--- Core_GamepadSupport/Navigation/CanvasCharmer.cs
+++ Core_GamepadSupport/Navigation/CanvasCharmer.cs
@@ -68,7 +68,8 @@ namespace KK_GamepadSupport.Navigation
             if (GamepadSupportPlugin.CanvasDebug.Value)
                 DrawCanvasList(currentSelectedGameObject);
 
-            CursorDrawer.DrawMousePointer(Input.mousePosition);
+            if (!Cursor.visible)
+                CursorDrawer.DrawMousePointer(Input.mousePosition);
         }
 
         private void Update()
